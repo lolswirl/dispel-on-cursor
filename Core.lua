d6ec62fb714d5cbf7dbc2bcaf675dcb43db0b3ca
Matt Riley <mjriley2@gmail.com>
2026-08-10 19:25:11 -0400

    Added support for non-healer specs

--- Core.lua
+++ Core.lua
@@ -1,10 +1,17 @@
 local DISPEL_SPELL_IDS = {
-    115450, -- detox (monk)
-    4987, -- cleanse (paladin)
-    527, -- purify (priest)
-    360823, -- naturalize (evoker)
-    88423, -- nature's cure (druid)
-    77130, -- purify spirit (shaman)
+    115450, -- detox (monk, heal)
+    218164, -- detox (monk, non-heal)
+    4987, -- cleanse (paladin, heal)
+    213644, -- cleanse toxins (paladin, non-heal)
+    527, -- purify (priest, heal)
+    213634, -- purify disease (priest, non-heal)
+    360823, -- naturalize (evoker, heal)
+    365585, -- expunge (evoker, non-heal)
+    88423, -- nature's cure (druid, heal)
+    2782, -- remove corruption (druid, non-heal)
+    77130, -- purify spirit (shaman, heal)
+    51886, -- cleanse spirit (shaman, non-heal)
+    475, -- remove curse (mage)
 }
 local TRACKED_SPELL_ID = nil
 
@@ -60,19 +67,27 @@ frame:SetScript("OnUpdate", function(self, elapsed)
     cooldownText:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", (x / scale) + 10, (y / scale) + 10)
 end)
 
+local function DetectDispelSpell()
+    for _, spellID in ipairs(DISPEL_SPELL_IDS) do
+        if C_SpellBook.IsSpellInSpellBook(spellID) then
+            TRACKED_SPELL_ID = spellID
+            UpdateCooldown()
+            return
+        end
+    end
+
+    TRACKED_SPELL_ID = nil
+    UpdateCooldown()
+end
+
 frame:RegisterEvent("PLAYER_LOGIN")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:RegisterEvent("SPELL_UPDATE_COOLDOWN")
+frame:RegisterEvent("TRAIT_CONFIG_UPDATED")
 
 frame:SetScript("OnEvent", function(self, event, ...)
-    if event == "PLAYER_LOGIN" or event == "PLAYER_ENTERING_WORLD" then
-        for _, spellID in ipairs(DISPEL_SPELL_IDS) do
-            if C_SpellBook.IsSpellInSpellBook(spellID) then
-                TRACKED_SPELL_ID = spellID
-                UpdateCooldown()
-                return
-            end
-        end
+    if event == "PLAYER_LOGIN" or event == "PLAYER_ENTERING_WORLD" or event == "TRAIT_CONFIG_UPDATED" then
+        DetectDispelSpell()
     elseif event == "SPELL_UPDATE_COOLDOWN" then
         UpdateCooldown()
     end
